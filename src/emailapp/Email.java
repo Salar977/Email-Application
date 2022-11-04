b868f57096cb1a42459aa7ac7c28b255022bb80f
Salar Amir <salar831@gmail.com>
2022-11-04 01:59:57 +0100

    Added getters Added Constructor

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,40 @@
 package emailapp;
 public class Email {
-    // Code for Email Application
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getMailboxCapacity() {
+        return mailboxCapacity;
+    }
+
+    private String alternateEmail;
+    private int mailboxCapacity;
+
+    // Constructor for first and last name
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Ask for the department
+
+    // Generate a random password
+
+    // Set mailbox Capacity
+
+    // Set the alternate email
+
+    // Change the password
+
+    //Write class API later :O
 }
